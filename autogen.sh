52aa3e3c55d2455be995e436c9ae70849cc5b578
Mark Lee <bzr@lazymalevolence.com>
2007-10-10 13:22:51 -0700

    2007-10-10  Mark Lee  <avant-wn@lazymalevolence.com>
    	* autogen.sh:
    	Replaced use of xdt-autogen with the individual
    	autotools-related scripts.
    	Remove required dependency on gnome-autogen.sh; autogen.sh
    	will use it if available, otherwise it will use the
    	autotools-related scripts individually.

--- autogen.sh
+++ autogen.sh
@@ -16,14 +16,28 @@ PKG_NAME="avant-window-navigator"
 if which gnome-autogen.sh ; then
   REQUIRED_AUTOMAKE_VERSION=1.9 . gnome-autogen.sh
 else
-  if which xdt-autogen ; then
-    . xdt-autogen
+  if which intltoolize && which glib-gettextize && which autoreconf ; then
+    intltoolize --copy --force --automake || \
+      (echo "There was an error in running intltoolize." > /dev/stderr;
+       exit 1)
+    glib-gettextize --copy --force || \
+      (echo "There was an error in running glib-gettextize." > /dev/stderr;
+       exit 1)
+    autoreconf --force --install || \
+      (echo "There was an error in running autoreconf." > /dev/stderr;
+       exit 1)
   else
     echo "No build script available.  You have two choices:"
     echo "1. You need to install the gnome-common module and make"
     echo "   sure the gnome-autogen.sh script is in your \$PATH."
-    echo "2. You need to install the xfce4-dev-tools module and make"
-    echo "   sure the xdt-autogen script is in your \$PATH."
+    echo "2. You need to install the following scripts:"
+    echo "   * intltool"
+    echo "   * glib-gettextize (usually installed with glib-2.0)"
+    echo "   * libtool"
+    echo "   * automake"
+    echo "   * autoconf"
+    echo "   Additionally, you need to make"
+    echo "   sure that they are in your \$PATH."
     exit 1
   fi
 fi
