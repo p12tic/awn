ca09d5fa7be3cc12b1cbd80537ff06c65ce943c5
Mark Lee <bzr@lazymalevolence.com>
2007-12-27 20:32:20 -0800

    Change required version for GLib's GChecksum; remove USE_EGG_CHECKSUM since GLIB_CHECK_VERSION is sufficient

--- libawn/awn-config-client.c
+++ libawn/awn-config-client.c
@@ -29,10 +29,10 @@
 #define AWN_GCONF_KEY_PREFIX "/apps/avant-window-navigator"
 #else
 #include <glib/gstdio.h>
-#ifdef USE_EGG_CHECKSUM 
-#include "egg/eggchecksum.h"
-#else
+#if GLIB_CHECK_VERSION(2,15,0)
 #include <glib/gchecksum.h>
+#else
+#include "egg/eggchecksum.h"
 #endif
 #endif
 
