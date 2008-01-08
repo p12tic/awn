1c85ef2660d983f407b7a72ef3de993a8cd8879a
Julien Lavergne <seagle@justice>
2008-01-08 01:07:06 +0100

    * awn-manager  - Use of tabs instead of a list in the Manager (Patch from bug #179666)
    * docs
     - Fix typo in the PPA_autoppa HowTo

--- awn-manager/awnManager.in.py
+++ awn-manager/awnManager.in.py
@@ -83,11 +83,6 @@ class AwnManager:
         close = self.wTree.get_widget("closebutton")
         close.connect("clicked", gtk.main_quit)
 
-        #menu
-        self.treeview =  self.wTree.get_widget("view_menu")
-        self.treeview.get_selection().connect("changed", self.menu_row_clicked)
-        self.make_menu_model()
-
         #theme
         self.themeManager = AwnThemeManager(self.wTree, self.AWN_CONFIG_DIR)
 
@@ -99,58 +94,6 @@ class AwnManager:
 
         #preferences
         self.prefManager = awnPreferences(self.wTree)
-
-    def menu_row_clicked(self, data=None):
-        selection = self.treeview.get_selection()
-        (model, iter) = selection.get_selected()
-        if iter != None:
-            self.show_panel(model.get_path(iter)[0])
-
-    def show_panel(self, index):
-        for panel in ['panel_preferences','panel_applets','panel_launchers','panel_themes']:
-            self.wTree.get_widget(panel).hide()
-        if index == 0:
-            self.wTree.get_widget('panel_preferences').show()
-        elif index == 1:
-            self.wTree.get_widget('panel_applets').show()
-        elif index == 2:
-            self.wTree.get_widget('panel_launchers').show()
-        elif index == 3:
-            self.wTree.get_widget('panel_themes').show()
-
-
-    def make_menu_model (self):
-        self.model = model = gtk.ListStore(gdk.Pixbuf, str, str, str)
-        self.treeview.set_model (model)
-
-        ren = gtk.CellRendererPixbuf()
-        col = gtk.TreeViewColumn ("Pixbuf", ren, pixbuf=0)
-        self.treeview.append_column (col)
-
-        ren = gtk.CellRendererText()
-        col = gtk.TreeViewColumn ("Name", ren, markup=1)
-        self.treeview.append_column (col)
-
-        theme = gtk.icon_theme_get_default()
-
-        row = self.model.append ()
-        self.model.set_value (row, 0, theme.load_icon (gtk.STOCK_PREFERENCES, 32, 0))
-        self.model.set_value (row, 1, "General")
-
-        row = self.model.append ()
-        self.model.set_value (row, 0, theme.load_icon (gtk.STOCK_SORT_ASCENDING, 32, 0))
-        self.model.set_value (row, 1, "Applets")
-
-        row = self.model.append ()
-        self.model.set_value (row, 0, theme.load_icon (gtk.STOCK_FULLSCREEN, 32, 0))
-        self.model.set_value (row, 1, "Launchers")
-
-        row = self.model.append ()
-        self.model.set_value (row, 0, theme.load_icon (gtk.STOCK_HOME, 32, 0))
-        self.model.set_value (row, 1, "Themes")
-
-        path = self.model.get_path(self.model.get_iter_first())
-        self.treeview.set_cursor(path, focus_column=None, start_editing=False)
 
     def refresh(self, button):
         w = gtk.Window()
