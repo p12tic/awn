49cdd338b5641d017046a87ea5c1097e763ade4f
moonbeam <rcryderman@gmail.com>
2008-03-09 13:00:25 -0400

    awn-effects.* 	-remvoed desaturate effect. Appears to be unused

--- libawn/awn-effects.c
+++ libawn/awn-effects.c
@@ -134,58 +134,6 @@ awn_effects_set_title (AwnEffects * fx, AwnTitle * title,
 
 
 static gboolean
-desaturate_effect (AwnEffectsPrivate * priv)
-{
-  AwnEffects *fx = priv->effects;
-  if (!fx->effect_lock)
-  {
-    fx->effect_lock = TRUE;
-    // effect start initialize values
-    fx->direction = AWN_EFFECT_DIR_DOWN;
-    fx->saturation = 1.0;
-    if (priv->start)
-      priv->start (fx->self);
-    priv->start = NULL;
-  }
-
-  const gdouble DESATURATION_STEP = 0.04;
-
-  switch (fx->direction)
-  {
-  case AWN_EFFECT_DIR_DOWN:
-    fx->saturation -= DESATURATION_STEP;
-    if (fx->saturation < 0)
-      fx->saturation = 0;
-    gboolean top = awn_effect_check_top_effect (priv, NULL);
-    // TODO: implement sleep function, so effect will stop the timer, but will be paused in middle of the animation and will finish when awn_effect_stop is called or higher priority effect is started
-    if (top)
-    {
-      gtk_widget_queue_draw (GTK_WIDGET (fx->self));
-      return top;
-    }
-    else
-      fx->direction = AWN_EFFECT_DIR_UP;
-    break;
-  case AWN_EFFECT_DIR_UP:
-  default:
-    fx->saturation += DESATURATION_STEP;
-  }
-
-  // repaint widget
-  gtk_widget_queue_draw (GTK_WIDGET (fx->self));
-
-  gboolean repeat = TRUE;
-  if (fx->saturation >= 1.0)
-  {
-    fx->saturation = 1.0;
-    // check for repeating
-    repeat = awn_effect_handle_repeating (priv);
-  }
-  return repeat;
-}
-
-
-static gboolean
 bounce_squish_effect (AwnEffectsPrivate * priv)
 {
   AwnEffects *fx = priv->effects;
@@ -1396,9 +1344,6 @@ main_effect_loop (AwnEffects * fx)
     break;
   case AWN_EFFECT_ATTENTION:
     animation = ATTENTION_EFFECTS[effects[4]];
-    break;
-  case AWN_EFFECT_DESATURATE:
-    animation = (GSourceFunc) desaturate_effect;
     break;
   default:
     return;
