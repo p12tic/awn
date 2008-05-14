21a20841ec6f4eb71cff8c7def2e3b114d21715f
moonbeam <rcryderman@gmail.com>
2008-05-14 16:59:37 -0400

    libawn/awn-effects.c: 	-added a test for a valid pixbuf.  Possible fix for https://bugs.launchpad.net/awn/+bug/226725

--- libawn/awn-effects.c
+++ libawn/awn-effects.c
@@ -2101,8 +2101,11 @@ void awn_draw_icons(AwnEffects *fx, cairo_t *cr, GdkPixbuf *icon, GdkPixbuf *ref
 			// refresh reflection, icon was clipped
 			if (!fx->delta_width && !fx->delta_height) {
 				// don't create reflection if we're also scaling
-				reflect = gdk_pixbuf_flip(clippedIcon, FALSE);
-				free_reflect = TRUE;
+      	if (GDK_IS_PIXBUF (clippedIcon) )//sanity check
+        {
+				  reflect = gdk_pixbuf_flip(clippedIcon, FALSE);
+  			  free_reflect = TRUE;          
+        }          
 			}
 			// adjust offsets
 			x1 = (fx->window_width - current_width) / 2;
