3a9325637089306ea0b3524df9c5f7ac90742478
moonbeam <rcryderman@gmail.com>
2008-07-28 20:13:41 -0400

    * libawn/awn-settings.[ch]:         Fixed a couple whitespace issues.

--- libawn/awn-settings.h
+++ libawn/awn-settings.h
@@ -18,107 +18,108 @@
  *
  *  Author : Neil Jagdish Patel <njpatel@gmail.com>
  *
- *  Notes : This is the actual icon on the app, the "Application Icon" 
+ *  Notes : This is the actual icon on the app, the "Application Icon"
 */
 
-#ifndef	_AWN_SETTINGS_H
-#define	_AWN_SETTINGS_H
+#ifndef _AWN_SETTINGS_H
+#define _AWN_SETTINGS_H
 
 #include <glib.h>
 #include <gtk/gtk.h>
 
 #include <libawn/awn-cairo-utils.h>
 
-typedef struct {
-	
-	/* Misc globals */
-	GtkIconTheme *icon_theme;
-	GtkWidget *bar;
-	GtkWidget *window;
-	GtkWidget *title;
-	GtkWidget *appman;
-	GtkWidget *hot;
-	gint task_width;
-	
-	/* monitor settings */
-	GdkRectangle monitor;
-	gboolean force_monitor;
-	int monitor_height;
-	int monitor_width;
-	gboolean panel_mode;
-	
-	gboolean auto_hide;
-	gboolean hidden;
-	gboolean hiding;
-	gint auto_hide_delay;
-	gboolean keep_below;
-	
-	int bar_height;
-	int bar_angle;
-	gfloat bar_pos;
-	gboolean no_bar_resize_ani;
+typedef struct
+{
 
-	/* Bar appearance settings */
-	gboolean rounded_corners;
-	gfloat corner_radius;
-	gboolean render_pattern;
-	gboolean expand_bar;
-	
-	gchar *pattern_uri;
-	gfloat pattern_alpha;
-	
-	AwnColor g_step_1;
-	AwnColor g_step_2;
-	AwnColor g_histep_1;
-	AwnColor g_histep_2;
-	AwnColor border_color;
-	AwnColor hilight_color;
-	
-	gboolean show_separator;
-	AwnColor sep_color;
-	
-	/* Window Manager Settings */
-	gboolean show_all_windows;
-	GSList *launchers;
-	
-	/* Task settings */
-	gboolean use_png;
-	gchar *active_png;
-	
-	AwnColor arrow_color;
-	int arrow_offset;
-	gboolean tasks_have_arrows;
-	
-	gboolean name_change_notify;
-	gboolean alpha_effect;
-	gint icon_effect;
-	float icon_alpha;
-	float reflection_alpha_mult;
-	gint frame_rate;
-	gboolean icon_depth_on;
-	
-	int icon_offset;
+  /* Misc globals */
+  GtkIconTheme *icon_theme;
+  GtkWidget *bar;
+  GtkWidget *window;
+  GtkWidget *title;
+  GtkWidget *appman;
+  GtkWidget *hot;
+  gint task_width;
+
+  /* monitor settings */
+  GdkRectangle monitor;
+  gboolean force_monitor;
+  int monitor_height;
+  int monitor_width;
+  gboolean panel_mode;
+
+  gboolean auto_hide;
+  gboolean hidden;
+  gboolean hiding;
+  gint auto_hide_delay;
+  gboolean keep_below;
+
+  int bar_height;
+  int bar_angle;
+  gfloat bar_pos;
+  gboolean no_bar_resize_ani;
+
+  /* Bar appearance settings */
+  gboolean rounded_corners;
+  gfloat corner_radius;
+  gboolean render_pattern;
+  gboolean expand_bar;
+
+  gchar *pattern_uri;
+  gfloat pattern_alpha;
+
+  AwnColor g_step_1;
+  AwnColor g_step_2;
+  AwnColor g_histep_1;
+  AwnColor g_histep_2;
+  AwnColor border_color;
+  AwnColor hilight_color;
+
+  gboolean show_separator;
+  AwnColor sep_color;
+
+  /* Window Manager Settings */
+  gboolean show_all_windows;
+  GSList *launchers;
+
+  /* Task settings */
+  gboolean use_png;
+  gchar *active_png;
+
+  AwnColor arrow_color;
+  int arrow_offset;
+  gboolean tasks_have_arrows;
+
+  gboolean name_change_notify;
+  gboolean alpha_effect;
+  gint icon_effect;
+  float icon_alpha;
+  float reflection_alpha_mult;
+  gint frame_rate;
+  gboolean icon_depth_on;
+
+  int icon_offset;
   int reflection_offset;
   int bar_depth_scale;
-	gboolean show_shadows;
-	
-	/* Title settings */
-	AwnColor text_color;
-	AwnColor shadow_color;
-	AwnColor background;
-	gchar *font_face;	
-	
-	gboolean btest;
-	float ftest;
-	char* stest;
-	AwnColor ctest;
-	GSList *ltest;
-	
-	/* Curves settings */
-	int bar_width;
-	gfloat curviness;
-	gfloat curves_symmetry;
-		
+  gboolean show_shadows;
+
+  /* Title settings */
+  AwnColor text_color;
+  AwnColor shadow_color;
+  AwnColor background;
+  gchar *font_face;
+
+  gboolean btest;
+  float ftest;
+  char* stest;
+  AwnColor ctest;
+  GSList *ltest;
+
+  /* Curves settings */
+  int bar_width;
+  gfloat curviness;
+  gfloat curves_symmetry;
+
 } AwnSettings;
 
 
