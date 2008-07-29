19eb1242b78841ab02c83f78e4194fe76da639ca
Henry Zhang <hua.zhang@sun.com>
2008-07-29 12:51:17 -0400

    * libawn/awn-config-client-shared.c:         Replace flock() with fcntl() as fcntl() is POSIX.  Also has the benefit that is works over NFS.         Patch submtted by:  Henry Zhang <hua.zhang@sun.com>.

--- libawn/awn-config-client-shared.c
+++ libawn/awn-config-client-shared.c
@@ -32,6 +32,13 @@
 #include "egg/eggchecksum.h"
 #endif
 
+#ifndef LOCK_SH
+#define   LOCK_SH   1    /* shared lock */
+#define   LOCK_EX   2    /* exclusive lock */
+#define   LOCK_NB   4    /* don't block when locking */
+#define   LOCK_UN   8    /* unlock */
+#endif
+
 static gpointer
 _awn_config_client_copy (gpointer boxed)
 {
@@ -90,7 +97,21 @@ int awn_config_client_key_lock_open (const gchar *group, const gchar *key)
  */
 int awn_config_client_key_lock (int fd, int operation)
 {
-	return flock (fd, operation);
+        struct flock f;
+
+        memset(&f, 0, sizeof (f));
+        if (operation & LOCK_UN)
+            f.l_type = F_UNLCK;
+        if (operation & LOCK_SH)
+            f.l_type = F_RDLCK;
+        if (operation & LOCK_EX)
+            f.l_type = F_WRLCK;
+        f.l_start = 0 ;
+        f.l_whence = SEEK_SET;
+        f.l_len = 0 ;
+        f.l_pid = getpid() ;
+
+        return fcntl(fd, (operation & LOCK_NB) ? F_SETLK : F_SETLKW, &f);
 }
 
 /**
