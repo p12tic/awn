8697e3f0de67fe4b3cf1439994cf45dc5e2e9a9e
moonbeam <moonbeam@rosie>
2009-02-09 10:28:27 -0500

    * libawn/awn-utils.c: * libawn/awn-utils.h:
    	A place for shared functions.  currently housing the make transparent function.

---
+++ libawn/awn-utils.c
@@ -0,0 +1,40 @@
+/*
+ *  Copyright (C) 2009 Michal Hruby <michal.mhr@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+#include <gdk/gdk.h>
+
+#include "awn-utility.h"
+
+gboolean
+awn_utility_make_transparent (GtkWidget *widget)
+{   
+  static GdkPixmap *pixmap = NULL;
+  if (gtk_widget_is_composited(widget))  
+  {
+    if (pixmap == NULL)
+    {
+      pixmap = gdk_pixmap_new(widget->window, 1, 1, -1);
+      cairo_t *cr = gdk_cairo_create(pixmap);
+      cairo_set_operator(cr, CAIRO_OPERATOR_CLEAR);
+      cairo_paint(cr);
+      cairo_destroy(cr);
+    }
+    gdk_window_set_back_pixmap(widget->window, pixmap, FALSE);    
+  }
+  return FALSE;
+}
