b073f18642e8ba57b0b855863cd6c7d7e78d74bd
Mark Lee <bzr@lazymalevolence.com>
2009-04-24 13:04:33 -0700

    * libawn/awn-cairo-utils.c: 	Fix variable shadowing errors.

--- libawn/awn-cairo-utils.c
+++ libawn/awn-cairo-utils.c
@@ -32,58 +32,58 @@
  * draws a rounded rectangle via cairo.
  */
 void
-awn_cairo_rounded_rect(cairo_t *cr, double x0, double y0,
+awn_cairo_rounded_rect(cairo_t *cr, double rx0, double ry0,
                        double width, double height,
                        double radius, AwnCairoRoundCorners state)
 {
-  double x1, y1;
+  double rx1, ry1;
 
-  x1 = x0 + width;
-  y1 = y0 + height;
+  rx1 = rx0 + width;
+  ry1 = ry0 + height;
 
   /* top left corner */
 
   if ((state & ROUND_TOP_LEFT) == ROUND_TOP_LEFT)
   {
-    cairo_move_to(cr, x0, y0 + radius);
-    cairo_curve_to(cr, x0 , y0, x0 , y0, x0 + radius, y0);
+    cairo_move_to(cr, rx0, ry0 + radius);
+    cairo_curve_to(cr, rx0 , ry0, rx0 , ry0, rx0 + radius, ry0);
   }
   else
   {
-    cairo_move_to(cr, x0, y0);
+    cairo_move_to(cr, rx0, ry0);
   }
 
   /* top right */
   if ((state & ROUND_TOP_RIGHT) == ROUND_TOP_RIGHT)
   {
-    cairo_line_to(cr, x1 - radius, y0);
-    cairo_curve_to(cr, x1, y0, x1, y0, x1, y0 + radius);
+    cairo_line_to(cr, rx1 - radius, ry0);
+    cairo_curve_to(cr, rx1, ry0, rx1, ry0, rx1, ry0 + radius);
   }
   else
   {
-    cairo_line_to(cr, x1, y0);
+    cairo_line_to(cr, rx1, ry0);
   }
 
   /* bottom right */
   if ((state & ROUND_BOTTOM_RIGHT) == ROUND_BOTTOM_RIGHT)
   {
-    cairo_line_to(cr, x1 , y1 - radius);
-    cairo_curve_to(cr, x1, y1, x1, y1, x1 - radius, y1);
+    cairo_line_to(cr, rx1 , ry1 - radius);
+    cairo_curve_to(cr, rx1, ry1, rx1, ry1, rx1 - radius, ry1);
   }
   else
   {
-    cairo_line_to(cr, x1, y1);
+    cairo_line_to(cr, rx1, ry1);
   }
 
   /* bottom left */
   if ((state & ROUND_BOTTOM_LEFT) == ROUND_BOTTOM_LEFT)
   {
-    cairo_line_to(cr, x0 + radius, y1);
-    cairo_curve_to(cr, x0, y1, x0, y1, x0, y1 - radius);
+    cairo_line_to(cr, rx0 + radius, ry1);
+    cairo_curve_to(cr, rx0, ry1, rx0, ry1, rx0, ry1 - radius);
   }
   else
   {
-    cairo_line_to(cr, x0, y1);
+    cairo_line_to(cr, rx0, ry1);
   }
 
   cairo_close_path(cr);
