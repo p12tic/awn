2f6a6c80d93017deb2c2f13fb36e4924d46eed8b
R. Cryderman <rcryderman@gmail.com>
2009-06-04 17:57:58 -0400

    * libawn/awn-overlay-icon.c:
    	Some properties for AwnOverlayIcon.

--- libawn/awn-overlay-icon.c
+++ libawn/awn-overlay-icon.c
@@ -27,8 +27,17 @@ G_DEFINE_TYPE (AwnOverlayIcon, awn_overlay_icon, AWN_TYPE_OVERLAY)
 
 typedef struct _AwnOverlayIconPrivate AwnOverlayIconPrivate;
 
-struct _AwnOverlayIconPrivate {
-    int dummy;
+struct _AwnOverlayIconPrivate 
+{
+    gdouble alpha;
+    gchar * icon_name;
+};
+
+enum
+{
+  PROP_0,
+  PROP_ALPHA,
+  PROP_ICON_NAME
 };
 
 static void 
@@ -42,9 +51,19 @@ static void
 awn_overlay_icon_get_property (GObject *object, guint property_id,
                               GValue *value, GParamSpec *pspec)
 {
-  switch (property_id) {
-  default:
-    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  AwnOverlayIconPrivate * priv;
+  priv = AWN_OVERLAY_ICON_GET_PRIVATE (object);
+
+  switch (property_id) 
+  {
+    case PROP_ALPHA:
+      g_value_set_double (value,priv->alpha);
+      break;
+    case PROP_ICON_NAME:
+      g_value_set_string (value,priv->icon_name);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
   }
 }
 
@@ -52,9 +71,20 @@ static void
 awn_overlay_icon_set_property (GObject *object, guint property_id,
                               const GValue *value, GParamSpec *pspec)
 {
-  switch (property_id) {
-  default:
-    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+  AwnOverlayIconPrivate * priv;
+  priv = AWN_OVERLAY_ICON_GET_PRIVATE (object);
+
+  switch (property_id) 
+  {
+    case PROP_ALPHA:
+      priv->alpha = g_value_get_double (value);
+      break;
+    case PROP_ICON_NAME:
+      g_free(priv->icon_name);
+      priv->icon_name = g_value_dup_string (value);
+      break;
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
   }
 }
 
@@ -74,8 +104,7 @@ static void
 awn_overlay_icon_class_init (AwnOverlayIconClass *klass)
 {
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
-
-  g_type_class_add_private (klass, sizeof (AwnOverlayIconPrivate));
+  GParamSpec   *pspec;      
 
   object_class->get_property = awn_overlay_icon_get_property;
   object_class->set_property = awn_overlay_icon_set_property;
@@ -84,7 +113,23 @@ awn_overlay_icon_class_init (AwnOverlayIconClass *klass)
   
   AWN_OVERLAY_CLASS(klass)->render_overlay = _awn_overlay_icon_render;
   
+  pspec = g_param_spec_double ("alpha",
+                               "alpha",
+                               "Alpha",
+                               0.0,
+                               1.0,
+                               0.9,
+                               G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
+  g_object_class_install_property (object_class, PROP_ALPHA, pspec);   
   
+  pspec = g_param_spec_string ("icon_name",
+                               "Icon name",
+                               "Icon gtk theme name",
+                               "",
+                               G_PARAM_READWRITE | G_PARAM_CONSTRUCT);
+  g_object_class_install_property (object_class, PROP_ICON_NAME, pspec);   
+  
+  g_type_class_add_private (klass, sizeof (AwnOverlayIconPrivate));  
 }
 
 static void
