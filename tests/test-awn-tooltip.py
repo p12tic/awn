0f36df89110d7c2a411583773ad808d1541132c1
Michal Hruby <michal.mhr@gmail.com>
2009-06-18 23:50:31 +0200

    reviewed by: mhr3 	patch by: onox 	* bindings/python/awn.override: 	Added python binding override for ThemedIcon.set_info 	* tests/test-awn-tooltip.py: 	Added test for ThemedIcon.set_info()

--- tests/test-awn-tooltip.py
+++ tests/test-awn-tooltip.py
@@ -4,28 +4,32 @@ import awn
 import gtk
 import gobject
 
-def show_hide_tooltip(tooltip):
+def show_hide_tooltip(icon):
+  tooltip = icon.get_tooltip()
   if (tooltip.flags() & gtk.VISIBLE) == 0:
     tooltip.update_position()
     tooltip.show()
+    icon.set_state("icon1")
   else:
     tooltip.hide()
+    icon.set_state("icon2")
 
   return True # timer repeats
 
 win = gtk.Window()
 box = gtk.HBox()
 icon = awn.ThemedIcon()
-icon.set_info_simple("media-player", "1234", "media-player")
+icon.set_info_simple("media-player", "12345", "media-player")
 icon.set_tooltip_text("Icon with smart-behavior & toggle-on-click")
 
 icon2 = awn.ThemedIcon()
-icon2.set_info_simple("media-player", "12345", "media-player")
+icon2.set_info("media-player", "1234", ["icon1", "icon2"], ["gnome-main-menu", "media-player"])
+icon2.set_state("icon2")
 icon2.set_tooltip_text("Icon with manual tooltip control")
 icon2.get_tooltip().set_position_hint("top", 35)
 icon2.get_tooltip().props.smart_behavior = False
 icon2.get_tooltip().props.toggle_on_click = False
-gobject.timeout_add(1500, show_hide_tooltip, icon2.get_tooltip())
+gobject.timeout_add(1500, show_hide_tooltip, icon2)
 
 icon3 = awn.ThemedIcon()
 icon3.set_info_simple("media-player", "123456", "media-player")
