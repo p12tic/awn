27d07b8a5340ec6cc18ef28dd63cc72d0fcd0496
R. Cryderman <rcryderman@gmail.com>
2010-01-08 17:49:57 -0500

    * applets/taskmanager/awn-desktop-lookup-cached.c: * applets/taskmanager/awn-desktop-lookup-cached.h: * applets/taskmanager/awn-desktop-lookup-gnome3.c: * applets/taskmanager/awn-desktop-lookup-gnome3.h: * applets/taskmanager/awn-desktop-lookup.c: * applets/taskmanager/awn-desktop-lookup.h: * libawn/awn-desktop-lookup-client.c: * libawn/awn-desktop-lookup-client.h:         Add license/copyright info.

--- libawn/awn-desktop-lookup-client.h
+++ libawn/awn-desktop-lookup-client.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2010 Rodney Cryderman <rcryderman@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License version 
+ * 2 or later as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authored by Rodney Cryderman <rcryderman@gmail.com>
+ *
+ */
 /* awn-desktop-lookup-client.h */
 
 #ifndef _AWN_DESKTOP_LOOKUP_CLIENT
