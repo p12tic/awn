758731359a2b06c73a30a1789375a2d7d0ff72b1
R. Cryderman <rcryderman@gmail.com>
2010-01-09 13:26:58 -0500

    * libawn/awn-dialog.c:         Revert change mistakenly committed in rev 1881.

--- libawn/awn-dialog.c
+++ libawn/awn-dialog.c
@@ -411,14 +411,6 @@ _expose_event (GtkWidget *widget, GdkEventExpose *expose)
   path = cairo_copy_path (cr);
   cairo_fill (cr);
 
-  /* Propagate the signal */
-  child = gtk_bin_get_child (GTK_BIN (widget));
-
-  if (child)
-    gtk_container_propagate_expose (GTK_CONTAINER (widget),
-                                    child, expose);
-
-  return TRUE; 
   cairo_save (cr);
 
   /* inner border, which is 2 pixels smaller */
