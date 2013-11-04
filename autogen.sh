54646249c9f6d47309cac247f82d66d1eea65bd8
Povilas Kanapickas <tir5c3@yahoo.co.uk>
2013-11-04 08:59:39 +0200

    Move configure.in to configure.ac per autoconf guidelines

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,7 @@ test -z "$srcdir" && srcdir=.
 
 PKG_NAME="avant-window-navigator"
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -f $srcdir/autogen.sh) || {
     echo -n "**Error**: Directory \`$srcdir' does not look like the"
     echo " top-level $PKG_NAME directory"
